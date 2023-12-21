2d2e935dae59c1d6dbf4fdc557e3701cbe915cf4
rana-daoud <109581481+rana-daoud@users.noreply.github.com>
2023-12-21 23:02:18 +0200

    add test-python-universal-aerospike-online to makefile

--- sdk/python/tests/integration/feature_repos/universal/online_store/aerospike.py
+++ sdk/python/tests/integration/feature_repos/universal/online_store/aerospike.py
@@ -11,19 +11,18 @@ from tests.integration.feature_repos.universal.online_store_creator import (
 class AerospikeOnlineStoreCreator(OnlineStoreCreator):
     def __init__(self, project_name: str, **kwargs):
         super().__init__(project_name)
-        # TODO
-        # self.container = DockerContainer("aerospike").with_exposed_ports("6379")
+        # TODO check if test container works for aerospike
+        self.container = DockerContainer("aerospike").with_exposed_ports("3000")
 
+    # TODO check if used and what should be the returned values in dict
     def create_online_store(self) -> Dict[str, str]:
-        pass
-        # self.container.start()
-        # log_string_to_wait_for = "Ready to accept connections"
-        # wait_for_logs(
-        #     container=self.container, predicate=log_string_to_wait_for, timeout=10
-        # )
-        # exposed_port = self.container.get_exposed_port("6379")
-        # return {"type": "aerospike", "connection_string": f"localhost:{exposed_port},db=0"}
+        self.container.start()
+        log_string_to_wait_for = "Ready to accept connections"
+        wait_for_logs(
+            container=self.container, predicate=log_string_to_wait_for, timeout=10
+        )
+        exposed_port = self.container.get_exposed_port("3000")
+        return {"type": "aerospike", "port": exposed_port}
 
     def teardown(self):
-        pass
-        # self.container.stop()
+        self.container.stop()
